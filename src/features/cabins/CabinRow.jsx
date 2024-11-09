b2c7d92768b10acddac22c5962e815daa0ceba0c
kunal <kunalparashar2555@gmail.com>
2024-11-09 17:58:21 +0530

    Building reusable table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -10,18 +10,20 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -79,7 +81,7 @@ function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row>
         <Img src={image} />
         <Cabin>{name}</Cabin>
         <div>Fits up tp {maxCapacity} guests</div>
@@ -112,8 +114,17 @@ function CabinRow({ cabin }) {
           ) : (
             ""
           )}
+          <Menus.Menu>
+            <Menus.Toggle id={cabinId} />
+
+            <Menus.List id={cabinId}>
+              <Menus.Button>Duplicate</Menus.Button>
+              <Menus.Button>Edit</Menus.Button>
+              <Menus.Button>Delete</Menus.Button>
+            </Menus.List>
+          </Menus.Menu>
         </div>
-      </TableRow>
+      </Table.Row>
       {/* {showForm && <CreateCabinForm cabinToEdit={cabin} />} */}
 
       {showForm && (
--- src/features/cabins/CabinTable copy.jsx
+++ src/features/cabins/CabinTable copy.jsx
@@ -6,7 +6,6 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -28,21 +27,18 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
   return (
-    <Menus>
-      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-        <Table.Header>
-          <div>CABIN</div>
-          <div>NAME</div>
-          <div>CAPICITY</div>
-          <div>PRICE</div>
-          <div>DISCOUNT</div>
-        </Table.Header>
-        <Table.Body
-          data={cabins}
-          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-        />
-      </Table>
-    </Menus>
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header>
+        <div>CABIN</div>
+        <div>NAME</div>
+        <div>CAPICITY</div>
+        <div>PRICE</div>
+        <div>DISCOUNT</div>
+      </Table.Header>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
   );
 }
 export default CabinTable;
