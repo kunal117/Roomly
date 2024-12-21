0ca122c1b2a947671f264c0c2e167c8f9d38b9c3
kunal <kunalparashar2555@gmail.com>
2024-12-21 12:07:13 +0530

    Building bookings table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -6,12 +6,15 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  if (!cabins.length) return <Empty resourceName={"cabins"} />;
 
   // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
@@ -26,11 +29,19 @@ function CabinTable() {
     filterCabins = cabins.filter((cabin) => cabin.discount > 0);
 
   // 2) SORT
-  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const sortBy = searchParams.get("sortBy") || "name-asc";
+
   const [field, direction] = sortBy.split("-");
+  // console.log(field, direction);
   const modifier = direction === "asc" ? 1 : -1;
-  const sortedCabins = filterCabins.sort(
-    (a, b) => (a[field] - b[field]) * modifier
+  // const sortedCabins = filterCabins.sort(
+  //   (a, b) => Number(a[field]) - Number(b[field]) * modifier
+  // );
+
+  const sortedCabins = filterCabins?.sort((a, b) =>
+    typeof a[field] === "string"
+      ? a[field].localeCompare(b[field]) * modifier
+      : (a[field] - b[field]) * modifier
   );
 
   return (
--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -17,8 +17,8 @@ function CabinTableOperations() {
         options={[
           { value: "name-asc", label: "Sort by name (A-Z)" },
           { value: "name-desc", label: "Sort by name (Z-A)" },
-          { value: "regularPrize-asc", label: "Sort by price (low first)" },
-          { value: "regularPrize-desc", label: "Sort by price (high first)" },
+          { value: "regularPrice-asc", label: "Sort by price (low first)" },
+          { value: "regularPrice-desc", label: "Sort by price (high first)" },
           { value: "maxCapacity-asc", label: "Sort by capacity (low first)" },
           { value: "maxCapacity-desc", label: "Sort by capacity (high first)" },
         ]}
